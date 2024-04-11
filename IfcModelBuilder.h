8badead61a6b3d0992a9be15b27ad0dcfa69560c
Richard Brice <37087370+RickBrice@users.noreply.github.com>
2024-04-11 16:02:56 -0700

    Added option for polygons and added color to girder segments

--- IfcModelBuilder.h
+++ IfcModelBuilder.h
@@ -57,11 +57,18 @@ public:
       Curve3dAndFootPrint
    };
 
+   enum class SweepProfile
+   {
+      Polyline, // use IfcPolyline for sweep profile
+      IndexPolyCurve // use IfcIndexPolyCurve for sweep profile
+   };
+
    Schema schema = Schema::Schema_4x3_add2;
    ModelElements model_elements = ModelElements::AlignmentAndBridge;
    AlignmentModel alignment_model = AlignmentModel::GradientCurve;
    Tangents tangents = Tangents::Line;
    Representations representations = Representations::Curve3dOnly;
+   SweepProfile sweep_profile = SweepProfile::Polyline;
 };
 
 ///////////////////////////////////////////////////////////////////////////
