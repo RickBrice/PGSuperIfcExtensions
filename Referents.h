4c0520f4ed42884596a2d8d9e6dfa6e0fad9372a
Richard Brice <37087370+RickBrice@users.noreply.github.com>
2025-06-30 15:22:07 -0700

    Updates for new validation service rules and pier referents

--- Referents.h
+++ Referents.h
@@ -31,8 +31,6 @@ void CreateReferents(IfcHierarchyHelper<Schema>& file, std::shared_ptr<WBFL::EAF
 {
    USES_CONVERSION;
 
-   typename aggregate_of<typename Schema::IfcObjectDefinition>::ptr alignment_referents(new aggregate_of<typename Schema::IfcObjectDefinition>());
-
    auto directrix = GetAlignmentDirectrix(file, options);
 
    GET_IFACE2(pBroker, IEAFDisplayUnits, pDisplayUnits);
@@ -58,8 +56,6 @@ void CreateReferents(IfcHierarchyHelper<Schema>& file, std::shared_ptr<WBFL::EAF
 
    // Create referent
    auto start_station_referent = new Schema::IfcReferent(IfcParse::IfcGlobalId(), nullptr, std::string("Start of alignment station"), boost::none, boost::none, referent_placement, nullptr, Schema::IfcReferentTypeEnum::IfcReferentType_STATION);
-   file.addEntity(start_station_referent);
-   alignment_referents->push(start_station_referent); // add to list of all alignment referents
 
    // Define properties for Pset_Stationing
    typename aggregate_of<typename Schema::IfcProperty>::ptr pset_station_properties(new aggregate_of<typename Schema::IfcProperty>());
@@ -76,53 +72,9 @@ void CreateReferents(IfcHierarchyHelper<Schema>& file, std::shared_ptr<WBFL::EAF
    auto rel_defines_by_properties = new Schema::IfcRelDefinesByProperties(IfcParse::IfcGlobalId(), nullptr, std::string("Relates start station properties to referent"), boost::none, referents, property_set);
    file.addEntity(rel_defines_by_properties);
 
-   // now do referents for each pier
-
-   if (options.model_elements == CIfcModelBuilderOptions::ModelElements::AlignmentAndBridge)
-   {
-      //
-   // Referents for pier locations
    //
-      GET_IFACE2(pBroker, IBridge, pBridge);
-      auto nPiers = pBridge->GetPierCount();
-      for (auto pierIdx = 0; pierIdx < nPiers; pierIdx++)
-      {
-         // referent position
-         auto pierStation = pBridge->GetPierStation(pierIdx);
-
-         auto point_on_alignment = new Schema::IfcPointByDistanceExpression(
-            new Schema::IfcLengthMeasure(pierStation - startStation),
-            boost::none, boost::none, boost::none,
-            directrix);
-         auto relative_placement = new Schema::IfcAxis2PlacementLinear(point_on_alignment, nullptr, nullptr);
-         auto referent_placement = new Schema::IfcLinearPlacement(nullptr, relative_placement, nullptr);
-
-         // create referent
-         std::ostringstream os;
-         os << "Station " << T2A(WBFL::COGO::Station(pierStation).AsString(station_format).c_str()) << " " << T2A(LABEL_PIER_EX(pBridge->IsAbutment(pierIdx), pierIdx));
-         auto referent = new Schema::IfcReferent(IfcParse::IfcGlobalId(), nullptr, os.str(), boost::none, boost::none, referent_placement, nullptr, Schema::IfcReferentTypeEnum::IfcReferentType_POSITION);
-         file.addEntity(referent);
-         alignment_referents->push(referent);
-
-         // create and assign Pset_Stationing
-         typename aggregate_of<typename Schema::IfcProperty>::ptr pset_station_properties(new aggregate_of<typename Schema::IfcProperty>());
-         pset_station_properties->push(new Schema::IfcPropertySingleValue(std::string("Station"), boost::none, new Schema::IfcLengthMeasure(pierStation), nullptr));
-
-         auto property_set = new Schema::IfcPropertySet(IfcParse::IfcGlobalId(), nullptr, std::string("Pset_Stationing"), boost::none, pset_station_properties);
-         file.addEntity(property_set);
-
-         typename aggregate_of<typename Schema::IfcObjectDefinition>::ptr referents(new aggregate_of<typename Schema::IfcObjectDefinition>());
-         referents->push(referent);
-
-         auto rel_defines_by_properties = new Schema::IfcRelDefinesByProperties(IfcParse::IfcGlobalId(), nullptr, std::string("Relates pier station properties to referent"), boost::none, referents, property_set);
-         file.addEntity(rel_defines_by_properties);
-      }
-   }
-
-   //
-   // Nest referents to alignment
+   // Nest the referent to alignment
    //
    auto alignment = file.getSingle<typename Schema::IfcAlignment>();
-   auto nests_stationing = new Schema::IfcRelNests(IfcParse::IfcGlobalId(), nullptr, std::string("Nests Referents with Alignment"), boost::none, alignment, alignment_referents);
-   file.addEntity(nests_stationing);
+   file.addRelatedObject<typename Schema::IfcRelNests>(alignment, start_station_referent);
 }
