3424b4972e2d9a996614be9494f2d2b8f05f8403
Richard Brice <37087370+RickBrice@users.noreply.github.com>
2026-04-10 17:04:34 -0700

    Adds option to model rebar per ACI 131

--- ExportOptions.cpp
+++ ExportOptions.cpp
@@ -56,6 +56,7 @@ void CExportOptions::DoDataExchange(CDataExchange* pDX)
 
 	DDX_Check_Bool(pDX, IDC_WORK_PLAN, options.include_work_plan);
 	DDX_Check_Bool(pDX, IDC_INCLUDE_REBAR, options.include_rebar);
+	DDX_Check_Bool(pDX, IDC_PER_ACI131, options.rebar_per_aci131);
 	DDX_Check_Bool(pDX, IDC_INCLUDE_CAMBER, options.include_camber);
 	DDX_Check_Bool(pDX, IDC_QUANTITIES, options.include_quantities);
 
